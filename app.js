92a70285d277f52a366abbf8f0800d3ecaf5338d
augustinavicius-vu <ignas.augustinavicius@mif.stud.vu.lt>
2023-06-06 17:07:08 +0200

    C16 užduoties įrodymas

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// C16
+function pirminis(skaicius) {
+    if (skaicius < 2) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(skaicius); i++) {
+        if (skaicius % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function palindromas(skaicius) {
+    const skaiciusString = String(skaicius);
+    const apverstasString = skaiciusString.split('').reverse().join('');
+    return skaiciusString === apverstasString;
+}
+
+function rastiPalindromus(nuo, iki) {
+    var palindromai = [];
+
+    for (var i = nuo; i < iki; i++) {
+        if (pirminis(i)) {
+            if (palindromas(i)) {
+                palindromai.push(i);
+            }
+        }
+    }
+
+    return palindromai;
+}
+
+var nuo = 1;
+var iki = 10000000;
+
+console.dir(rastiPalindromus(nuo, iki), {'maxArrayLength': iki});
+
